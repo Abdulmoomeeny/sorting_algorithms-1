88d9edf22d7771f33d56c5bf409e481dd947799b
Mubarek Seid <mubareksd@gmail.com>
2022-10-15 21:24:02 +0300

    task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -31,7 +31,7 @@ size_t partition(int *array, size_t size, int lo, int hi)
 	int i = lo - 1;
 	int j;
 
-	for (j = lo; j <= hi - 1; j++)
+	for (j = lo; j < hi; j++)
 	{
 		if (array[j] < pivot)
 		{
@@ -40,8 +40,13 @@ size_t partition(int *array, size_t size, int lo, int hi)
 			print_array(array, size);
 		}
 	}
-	swap(array, i + 1, hi);
-	print_array(array, size);
+
+	if (array[hi] < array[i + 1])
+	{
+		swap(array, i + 1, hi);
+		print_array(array, size);
+	}
+
 	return (i + 1);
 }
 
@@ -74,5 +79,7 @@ void recursive_quick_sort(int *array, size_t size, int lo, int hi)
 
 void quick_sort(int *array, size_t size)
 {
+	if (array == NULL || size < 2)
+		return;
 	recursive_quick_sort(array, size, 0, size - 1);
 }
